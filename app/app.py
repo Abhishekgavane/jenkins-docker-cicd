a56edbf89fdbd61ed125ef8070557c4a008f1309
Abhishekgavane <abhigavane16@gmail.com>
2026-09-23 07:40:59 +0000

    changes in app py

--- app/app.py
+++ app/app.py
@@ -2,13 +2,18 @@ from flask import Flask
 
 app = Flask(__name__)
 
+
 @app.route("/")
 def home():
-        return "Hello from Jenkins CI/CD!"
+     return "Hello from Jenkins CI/CD!"
 
-    @app.route("/health")
-    def health():
-            return "OK"
 
-        if __name__ == "__main__":
-                app.run(host="0.0.0.0", port=5000)
+@app.route("/health")
+def health():
+    return "OK"
+
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000)
+                ```
+
