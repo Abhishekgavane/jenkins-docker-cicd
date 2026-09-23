98a6165e74232d97a623dcc150047464a536e3d3
Abhishekgavane <abhigavane16@gmail.com>
2026-09-23 16:11:59 +0000

    changes in app.py for indent

--- app/app.py
+++ app/app.py
@@ -5,7 +5,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def home():
-     return "Hello from Jenkins CI/CD!"
+    return "Hello from Jenkins CI/CD!"
 
 
 @app.route("/health")
@@ -15,5 +15,3 @@ def health():
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
-                ```
-
